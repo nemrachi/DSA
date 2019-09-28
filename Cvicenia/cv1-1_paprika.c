8b74428c65cf515c60a7ad355b17df3de86c00c3
emarich <ema.richnakova@gmail.com>
2019-09-28 17:03:12 +0200

    paprikaDone

--- Cvicenia/cv1-1_paprika.c
+++ Cvicenia/cv1-1_paprika.c
@@ -5,17 +5,59 @@
 #define MAX_LEN 51
 
 int main() {
-    char line[MAX_LEN];
-    char *token;
+    char ch;
+    int scenarios, profit = 0, temp, prev = 0,
+    buyPrice = 0, sellPrice, business = 1; // 1 - buy      0 - sell
 
-    int scenarios, profit, temp, next, best;
-    printf("Zadaj pocet scenarov: ");
-    scanf("%d\n", &scenarios);
+    printf("Zadaj pocet scenarov:\n");
+    scanf(" %d\n", &scenarios);
 
-    for (int i = 0; i < scenarios; ++i) {
-        fgets(line, MAX_LEN, stdin);
-        token = strtok(line, "\n");
-        printf("%0.2f\n", ceil(strlen(token)/2));
+    while (scenarios--) {
+        while (1) {
+            scanf("%d%c", &temp, &ch);
+
+            if (business) {
+                if (prev) {
+                    if (prev < temp) {
+                        buyPrice = prev;
+                        prev = temp;
+                        business = 0;
+                    } else {
+                        prev = temp;
+                    }
+                } else {
+                    prev = temp;
+                }
+            } else {
+                if (prev < temp) {
+                    prev = temp;
+                } else {
+                    sellPrice = prev;
+                    prev = temp;
+                    business = 1;
+                    profit += sellPrice - buyPrice;
+                }
+            }
+
+            if (ch == '\n' || ch == '\t') {
+                if (!business) {
+                    if (temp > buyPrice) {
+                        profit += temp - buyPrice;
+                        printf("%d\n",profit);
+                        profit = 0;
+                        business = 1;
+                        prev = 0;
+                    }
+                } else {
+                    printf("%d\n",profit);
+                    profit = 0;
+                    business = 1;
+                    prev = 0;
+                }
+                break;
+            }
+        }
     }
+
     return 0;
 }

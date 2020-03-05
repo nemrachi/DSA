9792d76bf61e1c7dd0f27dce769cc56122740657
emarich <ema.richnakova@gmail.com>
2020-03-05 16:07:49 +0100

    changes

--- Cvicenia/cv1-3_dlazdice.c
+++ Cvicenia/cv1-3_dlazdice.c
@@ -1,93 +1,38 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include<stdio.h>
+#define SIZE 50
 
-//char *adding(char *str, char element) {
-//    int count = 0;
-//
-//    for (int i = 0; i < sizeof(str) ; ++i) {
-//        if (str[i] == '*') {
-//            count++;
-//        }
-//    }
-//
-//    char temp[100];
-//    strcpy(temp, str);
-//
-//    str = realloc(str, sizeof(str)+count+1);
-//    strcpy(str, "");
-//    strcpy(str, temp);
-//    strcpy(temp, "");
-//
-//    int size = sizeof(str);
-//
-//    int j;
-//    for (j = 0; j < size ; ++j) {
-//        if (str[j] == '*') {
-//            strncpy(temp, str+j, size - j);
-//            str[j] = element;
-//            strcat(&str[j++], temp);
-//        }
-//    }
-//
-//    str[j] = '\0';
-//
-//    return str;
-//}
-//
-//char *recursion(int n, char *str) {
-//    char *temp = malloc(100);
-//    if (n == 1) {
-//        strcat(temp, "|*\0");
-//        return temp;
-//    } else if (n == 2) {
-//        strcat(temp, "||*=*\0");
-//        return temp;
-//    } else {
-//        str = strcat(adding(recursion(n-1, temp), '|'),
-//                     adding(recursion(n-2, temp), '='));
-//        return (str);
-//    }
-//
-//}
-//
-void foo (int *ver, int *hor, int n) {
-    if ((*hor*2) == n) {
-        for (int i = 0; i < *hor; ++i) {
-            printf("=");
-        }
-        *hor -= 1;
-        *ver += 2;
-    }
+char tiles[SIZE];
+int count = 0;
 
-//    if ((*hor*2)+*ver == n) {
-//        for (int i = 0; i < n; ++i) {
-//            for (int j =  0; j < ; ++j) {
-//
-//            }
-//        }{
-//
-//        }
-//    }
+void print_tiles(int n, int index) {
+	if (n >= 2) {
+		tiles[index] = '=';
+		print_tiles(n - 2, index + 1);
+	}
+    
+	if (n >= 1) {
+		tiles[index] = '|';
+		print_tiles(n - 1, index + 1);
+	}
+	
+	else {
+		for (int i = 0; i < index; ++i) {
+			printf("%c", tiles[i]);
+		}
+        count++;
+		printf("\n");
+	}
 }
 
 int main() {
     int input;
-    int vertical, horizontal;
-//    char *str = malloc(100);
-//
-    printf("Zadaj druhy rozmer 2x");
-    scanf("%d\n", &input);
+    scanf("%d", &input);
 
-    horizontal = input / 2;
-    vertical = input % 2;
-
-    while (horizontal == 0) {
-        foo(&vertical, &horizontal, input);
+    if (input >= 25) {
+        return 0;
     }
-//
-//    printf("%s", recursion(input, str));
-//
-//    free(str);
+
+	print_tiles(input, 0);
+    printf("%d\n", count);
     return 0;
 }
--- Cvicenia/ul2-2.c
+++ Cvicenia/ul2-2.c
@@ -1,73 +1,73 @@
 // uloha2-2.c -- Ema Richnáková, 17.11.2019 15:34
-
 #include <stdio.h>
+#include <stdlib.h>
 
-// Utriedi n cisel v poli a
-void utried(int *a, int n)
-{
-  // ...
+int get_max(int * a, int n) {
+  int max = a[0]; 
+
+  for (int i = 1; i < n; i++) { 
+      if (a[i] > max) {
+          max = a[i]; 
+      }
+  }
+
+  return max; 
 }
 
-int main(void)
-{
-  int i, *x, n;
+void count_sort(int * a, int n, int exp) { 
+    int output[n], i, count[10] = {0}; 
+  
+    for (i = 0; i < n; i++) {
+      count[(a[i] / exp) % 10]++; 
+    }
+   
+    for (i = 1; i < 10; i++) {
+      count[i] += count[i - 1];
+    } 
+
+    for (i = n - 1; i >= 0; i--) { 
+        output[count[(a[i] / exp) % 10] - 1] = a[i]; 
+        count[(a[i] / exp) % 10 ]--; 
+    } 
+  
+    for (i = 0; i < n; i++) {
+      a[i] = output[i]; 
+    }
+        
+} 
+
+// Utriedi n cisel v poli a
+void utried(int *a, int n) {
+  int max = get_max(a, n); 
+  
+  for (int i = 1; max / i > 0; i *= 10) {
+    count_sort(a, n, i); 
+  }
+    
+}
+
+int main(void) {
+  int i, * x, n;
 
   scanf("%d", &n);
-  x = (int*)malloc(n * sizeof(int));
-  for (i = 0; i < n; i++)
+  x = (int *)malloc(n * sizeof(int));
+
+  for (i = 0; i < n; i++) {
     scanf("%d", &x[i]);
+  }
 
   utried(x, n);
   
   printf("%d", x[0]);
-  for (i = 1; i < n; i++)
-  {
+
+  for (i = 1; i < n; i++) {
     printf(" %d", x[i]);
-    if (x[i-1] > x[i])
-    {
+    if (x[i-1] > x[i]) {
       printf(" -- CHYBA\n");
       return 0;
     }
   }
+
   printf("\n");
   return 0;
 }
-
-
-
-
-
-/*
-#include <stdio.h>
-
-// Utriedi n cisel v poli a
-void utried(int *a, int n)
-{
-  // ...
-}
-
-int main(void)
-{
-  int i, *x, n;
-
-  scanf("%d", &n);
-  x = (int*)malloc(n * sizeof(int));
-  for (i = 0; i < n; i++)
-    scanf("%d", &x[i]);
-
-  utried(x, n);
-  
-  printf("%d", x[0]);
-  for (i = 1; i < n; i++)
-  {
-    printf(" %d", x[i]);
-    if (x[i-1] > x[i])
-    {
-      printf(" -- CHYBA\n");
-      return 0;
-    }
-  }
-  printf("\n");
-  return 0;
-}
-*/
